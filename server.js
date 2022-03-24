bf36faacc6be84b9539a23365e6ed0b765495c44
Joss COUPET <contactpro.joss@gmail.com>
2022-03-24 10:08:04 +0100

    🔒️ add router security

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const moment = require('moment');
 
 const app = require("./conf/express");
 const mongo = require("./conf/mongo");
-const logger = require('./conf/logger');
+const logger = require("./conf/logger");
 
-const auth = require('./router/authRoute');
-const file = require('./router/fileRoute');
+const routerUtils = require("./utils/routerUtils");
+
+const auth = require("./router/authRoute");
+const file = require("./router/fileRoute");
 
 mongo.connect();
 
@@ -16,15 +18,25 @@ let BLACKLIST =["178.20.55.18"];
 app.all('*', function (req, res, next) {
     let ipAddress = req.ipInfo.ip.substr(0, 7) == "::ffff:" ? req.ipInfo.ip.substr(7) : req.ipInfo.ip;
     if (BLACKLIST.indexOf(ipAddress) === -1) {
-        console.log(`🔵 ${ipAddress} called route ${req.originalUrl} (🕦 ${moment().format('l')} ${moment().format('LTS')})`);
-        next();
-    } else {
-        res.send(ipAddress + ' IP is not in whiteList')
+        if (!routerUtils.isAuthorizedRoute(req)) {
+            res.status(403).json({
+                "success": false,
+                "data": { "message": "Unauthorized" }
+            });
+        } else {
+            console.log(`🔵 ${ipAddress} called route ${req.originalUrl} (🕦 ${moment().format('l')} ${moment().format('LTS')})`);
+            next();
+        }
+    } else {    
+        res.status(403).json({
+            "success": false,
+            "data": { "message": ipAddress + " IP is not in whiteList" }
+        });
     }
 });
 
 app.use("/users", auth);
-app.use("/file", file);
+app.use("/files", file);
 
 app.listen(port, () => {
     console.log(`🤖 Application running in: ${env}`)
