3498889e8122c85516682dc58c4f2b09e1b98df9
Joss COUPET <contactpro.joss@gmail.com>
2022-03-24 10:14:28 +0100

    🐛 lil fix

--- conf/mongo.js
+++ conf/mongo.js
@@ -6,7 +6,7 @@ mongoose.Promise = global.Promise;
 exports.connect = () => {
     mongoose.connect(mongoose_uri, {useNewUrlParser: true, useUnifiedTopology: true}, (error) => {
         if (!error) {
-            console.log("✅ MongoDB Connection Succeeded. URL: " + process.env.MONGO_DB_URL);
+            console.log("✅ MongoDB Connection Succeeded. URL: " + mongoose_uri);
         } else {
             console.log("Error in DB connection: " + error);
         }
